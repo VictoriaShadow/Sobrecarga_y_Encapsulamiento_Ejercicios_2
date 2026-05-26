d8c3473b01c7d1d379a8d2c7384d29a38c3e5599
Valen <maidanavalentina2008@gmail.com>
2026-05-26 04:59:28 -0300

    Ejercicio 2

---
+++ Ejercicio2/Ejercicio2_SimuladorTVSmart.cs
@@ -0,0 +1,181 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio_2_sobrecarga_y_encapsulamiento
+{
+    public class SmartTV
+    {
+        private string _marca;
+        private int _pulgadas;
+        private bool _encendido;
+        private int _canalActual;
+        private int _volumen;
+        private bool _esPremium;
+
+        public string Marca
+        {
+            get { return _marca; }
+            set
+            {
+                if (value != null)
+                    _marca = value;
+            }
+        }
+        public int Pulgadas
+        {
+            get { return _pulgadas; }
+            set
+            {
+                if (value != 0)
+                    _pulgadas = value;
+            }
+        }
+        public bool Encendido
+        {
+            get { return _encendido; }
+            set
+            {
+                _encendido = value;
+                if (_encendido)
+                {
+                    Console.WriteLine("Smart TV encendida");
+                }
+                else
+                {
+                    Console.WriteLine("Smart TV apagada");
+                }
+            }
+
+        }
+        public int CanalActual
+        {
+            get { return _canalActual; }
+            set
+            {
+                if (value >= 1)
+                    _canalActual = value;
+            }
+        }
+        public int Volumen
+        {
+            get { return _volumen; }
+            set
+            {
+                if (value < 100)
+                {
+                    _volumen = value;
+                }
+            }
+        }
+        public bool EsPremium
+        {
+            get { return _esPremium; }
+            set { _esPremium = value; }
+        }
+
+        public string CodigoConfig
+        {
+            get
+            {
+                string codigoConfig = "";
+                codigoConfig += _marca.ToUpper();
+                codigoConfig += "-" + _pulgadas + "-";
+                if (_esPremium)
+                {
+                    codigoConfig += "PREM";
+                }
+                else
+                {
+                    codigoConfig += "STD";
+                }
+                return codigoConfig;
+            }
+        }
+
+        public void Power()
+        {
+            _encendido = !_encendido;
+        }
+        public void CambiarCanal(int canal)
+        {
+            int max = 0;
+            if (_esPremium)
+            {
+                max = 500;
+            }
+            else
+            {
+                max = 100;
+            }
+            if (_encendido)
+            {
+                _canalActual = canal;
+
+                if (_canalActual > max)
+                {
+                    _canalActual = 1;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Se requiere que la TV este encendida");
+            }
+        }
+        public void CambiarCanal()
+        {
+            int max = 0;
+            if (_esPremium)
+            {
+                max = 500;
+            }
+            else
+            {
+                max = 100;
+            }
+            if (_encendido)
+            {
+                _canalActual += 1;
+                if (_canalActual > max)
+                {
+                    _canalActual = 1;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Se requiere que la TV este encendida");
+            }
+        }
+        public void RegularVolumen(bool volumen)
+        {
+            if (_encendido)
+            {
+                if (volumen)
+                {
+                    _volumen += 2;
+                }
+                else
+                {
+                    _volumen -= 2;
+                }
+                if (_volumen > 100)
+                {
+                    _volumen = 100;
+                }
+                else
+                {
+                    if (_volumen <= 0)
+                    {
+                        _volumen = 0;
+                        Console.WriteLine("MUTE");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Se requiere que la TV este encendida");
+            }
+        }
+
+    }
+}
---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,87 @@
+﻿using Ejercicio_2_sobrecarga_y_encapsulamiento;
+Console.WriteLine("Ingrese la marca: ");
+string marca = Console.ReadLine();
+Console.WriteLine("¿De cuantas pulgadas es la TV?");
+int pulgadas = int.Parse(Console.ReadLine());
+Console.WriteLine("¿Tienes plan premium? (S/N)");
+string respuestaPlan = Console.ReadLine();
+bool planPremium;
+if (respuestaPlan.ToUpper() == "S")
+{
+    planPremium = true;
+}
+else
+{
+    planPremium = false;
+}
+SmartTV tv1 = new SmartTV();
+tv1.Marca = marca;
+tv1.Pulgadas = pulgadas;
+tv1.EsPremium = planPremium;
+
+while (true)
+{
+    string volumen = "";
+    if (tv1.Volumen <= 0)
+    {
+        volumen = "MUTE";
+    } else
+    {
+        volumen = tv1.Volumen.ToString();
+    }
+    Console.Clear();
+    Console.WriteLine($"Identificador de la Smart TV: {tv1.CodigoConfig}");
+    Console.WriteLine("=============================================================");
+    if (tv1.Encendido)
+    {
+        Console.WriteLine($"Tv: [ ON ] | Canal actual: {tv1.CanalActual} | Volumen: {volumen}");
+    }
+    else
+    {
+        Console.WriteLine("Tv: [ OFF ]");
+    }
+    Console.WriteLine("=============================================================");
+    Console.WriteLine("[1] Encender o apagar televisor");
+    Console.WriteLine("[2] Cambiar canal");
+    Console.WriteLine("[3] Subir canal");
+    Console.WriteLine("[4] Regular volumen");
+    Console.WriteLine("[0] Salir del simulador");
+    Console.WriteLine("=============================================================");
+    Console.WriteLine("Seleccióne una opción del menú:_");
+    int op = int.Parse(Console.ReadLine());
+    switch (op)
+    {
+        case 0:
+            return;
+        case 1:
+            tv1.Power();
+            break;
+        case 2:
+            Console.WriteLine("Ingrese a que canal quiere cambiar");
+            int canal = int.Parse(Console.ReadLine());
+            tv1.CambiarCanal(canal);
+            break;
+        case 3:
+            tv1.CambiarCanal();
+            break;
+        case 4:
+            Console.WriteLine("¿Quiere subir o bajar el volumen? SUBIR / BAJAR");
+            string respuesta = Console.ReadLine();
+            bool cambio;
+            if (respuesta.ToUpper() == "SUBIR")
+            {
+                cambio = true;
+            }
+            else
+            {
+                cambio = false;
+            }
+            tv1.RegularVolumen(cambio);
+            break;
+        default:
+            Console.WriteLine("Opción inválida");
+            break;
+    }
+    Console.WriteLine("\nSeleccione cualquier letra para continuar");
+    Console.ReadKey();
+}
