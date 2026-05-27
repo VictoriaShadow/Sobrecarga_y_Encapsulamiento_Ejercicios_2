4a3be411ba49edab11d4f58f99e95fb65b172f18
Valen <maidanavalentina2008@gmail.com>
2026-05-26 22:51:59 -0300

    Ejercicio 4

---
+++ Ejercicio4/Ejercicio4_SimuladorInvernaderoInteligente.cs
@@ -0,0 +1,183 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio_4_sobrecarga_y_encapsulamiento
+{
+    public class Invernadero
+    {
+        private string _nombreSector;
+        private int _temperaturaActual;
+        private int _humedadSuelo;
+        private bool _sistemaRiegoActivo;
+        private bool _calefaccionActiva;
+        private string _tipoCultivo;
+
+        public string NombreSector
+        {
+            get { return _nombreSector; }
+            set
+            {
+                if (value != null)
+                    _nombreSector = value;
+            }
+        }
+        public int TemperaturaActual
+        {
+            get { return _temperaturaActual; }
+            set
+            {
+                if (value != 0)
+                    _temperaturaActual = value;
+            }
+        }
+        public int HumedadSuelo
+        {
+            get { return _humedadSuelo; }
+            set
+            {
+                if (value > 1 && value < 100)
+                {
+                    _humedadSuelo = value;
+                }
+                
+            }
+
+        }
+        public bool SistemaDeRiego
+        {
+            get { return _sistemaRiegoActivo; }
+            set
+            {
+                    _sistemaRiegoActivo = value;
+            }
+        }
+        public bool Calefaccion { 
+            get { return _calefaccionActiva; } 
+            set 
+            { 
+                    _calefaccionActiva = value;
+            } 
+        }
+        public string TipoCultivo
+        {
+            get { return _tipoCultivo; }
+            set { 
+                if (value.ToUpper() == "TROPICAL" || value.ToUpper() == "DESERTICO")
+                {
+                    _tipoCultivo = value.ToUpper();
+                }
+                }
+        }
+
+        public string Reporte_Estado
+        {
+            get
+            {
+                string reporteEstado = "";
+                reporteEstado += "SECTOR: " + _nombreSector.ToUpper();
+                reporteEstado += " - " + _tipoCultivo;
+                if (_tipoCultivo == "TROPICAL")
+                {
+                    if (_humedadSuelo < 60)
+                    {
+                        reporteEstado += " - ALERTA: BAJA HUMEDAD";
+                    } else
+                    {
+                        if (_temperaturaActual < 20 || _temperaturaActual > 28)
+                        {
+                            reporteEstado += " - ALERTA: TEMPERATURA INADECUADA";
+                        }
+                    }
+                } else
+                {
+                    if (_tipoCultivo == "DESERTICO")
+                    {
+                        if (_humedadSuelo > 20)
+                        {
+                            reporteEstado += " - ALERTA: ALTA HUMEDAD";
+                        }
+                        else
+                        {
+                            if (_temperaturaActual < 25 || _temperaturaActual > 35)
+                            {
+                                reporteEstado += " - ALERTA: TEMPERATURA INADECUADA";
+                            }
+                        }
+                    }
+                }
+                return reporteEstado;
+            }
+        }
+
+        public void SimularClima(int humedad, int temperatura)
+        {
+            if (_humedadSuelo == 100)
+            {
+                _sistemaRiegoActivo = false;
+                _humedadSuelo = humedad;
+            } else
+            {
+                _humedadSuelo = humedad;
+            }
+            if (_temperaturaActual > 45)
+            {
+                _calefaccionActiva = false;
+                _temperaturaActual = temperatura;
+            } else
+            {
+                _temperaturaActual = temperatura;
+            }
+        }
+        public void SimularClima()
+        {
+            _humedadSuelo -= 5;
+            _temperaturaActual += 1;
+        }
+        public void ControlAutomatico()
+        {
+            if (_tipoCultivo == "TROPICAL")
+            {
+                if (_humedadSuelo < 60)
+                {
+                    Console.WriteLine("Activando sistema de riego");
+                    _sistemaRiegoActivo = true;
+                }
+                else
+                {
+                    _sistemaRiegoActivo = false;
+                }
+                if (_temperaturaActual < 20 || _temperaturaActual > 28)
+                {
+                    Console.WriteLine("Activando sistema de calefacción");
+                    _calefaccionActiva = true;
+                } else
+                {
+                    _calefaccionActiva = false;
+                }
+            } else
+            {
+                if (_tipoCultivo == "DESERTICO")
+                {
+                    if (_humedadSuelo > 20)
+                    {
+                        Console.WriteLine("Recalibrando sistema de riego");
+                        _sistemaRiegoActivo = true;
+                    }
+                    else
+                    {
+                        _sistemaRiegoActivo = false;
+                    }
+                    if (_temperaturaActual < 25 || _temperaturaActual > 35)
+                    {
+                        Console.WriteLine("Activando sistema de calefacción");
+                        _calefaccionActiva = true;
+                    } else
+                    {
+                        _calefaccionActiva = false;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Ejercicio4/Program.cs
@@ -0,0 +1,75 @@
+﻿using Ejercicio_4_sobrecarga_y_encapsulamiento;
+Console.WriteLine("Ingrese nombre del sector");
+string nombreSector = Console.ReadLine();
+Console.WriteLine("Ingrese tipo de cultivo");
+string tipoCultivo = Console.ReadLine();
+Console.WriteLine("Ingrese temperatura del sector");
+int temperaturaSector = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese humedad del sector");
+int humedadSector = int.Parse(Console.ReadLine());
+Invernadero invernadero1 = new Invernadero();
+invernadero1.NombreSector = nombreSector;
+invernadero1.TipoCultivo = tipoCultivo;
+invernadero1.TemperaturaActual = temperaturaSector;
+invernadero1.HumedadSuelo = humedadSector;
+
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine($"Reporte de Estado: \n {invernadero1.Reporte_Estado}");
+    Console.WriteLine("========================================");
+    Console.WriteLine($"Temperatura actual: {invernadero1.TemperaturaActual} | Humedad del piso: {invernadero1.HumedadSuelo}");
+    string sistemaRiego = "";
+    string sistemaCalefaccion = "";
+    if (invernadero1.SistemaDeRiego)
+    {
+        sistemaRiego = "Activo";
+    } else
+    {
+        sistemaRiego = "Inactivo";
+    }
+    if (invernadero1.Calefaccion)
+    {
+        sistemaCalefaccion = "Activo";
+    }
+    else
+    {
+        sistemaCalefaccion = "Inactivo";
+    }
+    Console.WriteLine($"Sistema de riego: {sistemaRiego} | Sistema de calefaccion: {sistemaCalefaccion}");
+    Console.WriteLine("======================================== ");
+    
+    Console.WriteLine("[1] Forzar clima");
+    Console.WriteLine("[2] Dejar pasar el tiempo");
+    Console.WriteLine("[3] Ejecutar chequeo automático");
+    Console.WriteLine("[0] Salir del simulador");
+    Console.WriteLine("========================================");
+    Console.WriteLine("Seleccione una opción del control: _");
+    int op = int.Parse(Console.ReadLine());
+    switch (op)
+    {
+        case 0:
+            return;
+        case 1:
+            Console.WriteLine("Ingrese la humedad que quiera ingresar");
+            int humedad = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la temperatura que quiera agregar");
+            int temperatura = int.Parse(Console.ReadLine());
+            invernadero1.SimularClima(humedad, temperatura);
+            break;
+
+        case 2:
+            invernadero1.SimularClima();
+            break;
+
+        case 3:
+            invernadero1.ControlAutomatico();
+            break;
+
+        default:
+            Console.WriteLine("Opción inválida");
+            break;
+    }
+    Console.WriteLine("\n Seleccione cualquier letra para continuar");
+    Console.ReadKey();
+}
